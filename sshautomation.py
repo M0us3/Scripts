23385eea41b6981a05ed0c6319ba42bea41ced77
Nicolas Lindström <nicolaslindstrom1@gmail.com>
2022-07-01 15:54:27 -0700

    Create sshautomation.py
    # Written by Nick Lindstrom
    # Date 1-July-2022
    # Version 0.5
    
    # Requirements to run script, Python with installed modules Paramiko, SCP, and Getpass
    
    # Purpose of this script is to automate installation of application on remote host
    # This script will use SCP to copy folders and files from the host to remote host
    # Paramiko is used to establish SSH connection and send commands to remote host
    # Getpass is used to hide credentials that is entered

---
+++ sshautomation.py
@@ -0,0 +1,22 @@
+# Import modules and classes
+from paramiko import SSHClient, AutoAddPolicy
+from scp import SCPClient
+import getpass
+
+# hostname, credentials, and local folder
+files = ("folder location")
+hostname = input("Hostname: ")
+username = input("Username: ")
+password = input("Password: ")
+
+
+# Connect to host
+
+
+# Copy files to remote host
+
+
+# Run commands on remote host
+
+
+# Disconnect
