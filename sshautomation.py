593f8aed19a3bd3f196ed03153c32e511d492666
Nicolas Lindström <nicolaslindstrom1@gmail.com>
2022-09-22 17:07:55 -0700

    Update sshautomation.py
    Version 0.5
    Simple script without logic.
    Future changes will include logic, error checking and logging.

--- sshautomation.py
+++ sshautomation.py
@@ -1,5 +1,5 @@
 # Written by Nick Lindstrom
-# Date 1-July-2022
+# Date September-2022
 # Version 0.5
 
 # Requirements to run script, Python with installed modules Paramiko, SCP, and Getpass
@@ -17,13 +17,13 @@ from scp import SCPClient
 import getpass
 
 # Credentials for remote host connection
-print("Enter credentials that are used on the remote host")
+print("Enter credentials for the remote host")
 hostname = input("Hostname: ")
 username = input("Username: ")
-password = getpass("Password: ")
+password = getpass.getpass("Password: ")
 
 # Connect to host with ssh
-print("Connecting to " &hostname)
+print("Connecting to " + hostname)
 client = SSHClient()
 client.set_missing_host_key_policy(AutoAddPolicy())
 client.load_system_host_keys()
@@ -32,13 +32,13 @@ client.connect(hostname, username=username, password=password)
 # Verify folders are correct
 
 # Copy files to remote host
-print("Enter the local folder path and destination path that you want to copy")
+print("Enter the local folder path and destination path that you want to copy to")
 
 local_folder = input("Local path: ")
 
 remote_folder = input("Remote folder path: ")
 
-scp = SCPClient(ssh.get_transport())
+scp = SCPClient(client.get_transport())
 scp.put(local_folder, recursive=True, remote_path=remote_folder)
 
 scp.close()
@@ -49,4 +49,4 @@ scp.close()
 # Run commands on remote host
 
 
-# Disconnectnect
+# Disconnect
