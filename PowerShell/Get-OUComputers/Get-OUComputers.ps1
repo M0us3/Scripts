60350294d9a102754915d32f606da2248014be70
Nicolas Lindström <nicolaslindstrom1@gmail.com>
2023-02-21 09:38:13 -0800

    Update Get-OUComputers.ps1

--- PowerShell/Get-OUComputers/Get-OUComputers.ps1
+++ PowerShell/Get-OUComputers/Get-OUComputers.ps1
@@ -1,7 +1,10 @@
-$OU = ""
+# Set the OU in ldap format
+$OU = "OU=,DC=compnay,DC=com"
+
+# Create an array and get the computers that are located in the OU
 $Computers = @(Get-adcomputer -filter * -searchbase $OU | fl -property Name | out-string)
 
+# Print each computer
 foreach ($computer in $computers) {
 	write-host $computer
-
 }
