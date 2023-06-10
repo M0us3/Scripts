488ecab9e45109e6a19f4a03187fcf490b33d7f2
Nicolas Lindström <nicolaslindstrom1@gmail.com>
2023-06-10 14:15:48 -0700

    Create HeaderTemplate.ps1

---
+++ PowerShell/Header Template/HeaderTemplate.ps1
@@ -0,0 +1,19 @@
+<#
+.SYNOPSIS
+  Create local administrator
+.DESCRIPTION
+ Descripton of script
+.PARAMETER Param1
+  N/A
+.INPUTS
+  N/A
+.OUTPUTS
+  N/A
+.NOTES
+  Version:        1
+  Author:         Nick Lindstrom
+  Creation Date:  
+  Purpose/Change: 
+.EXAMPLE
+  N/A
+#>
