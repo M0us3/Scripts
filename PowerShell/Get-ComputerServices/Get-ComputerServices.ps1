35df9ca81c9987e6677a21ed21d4ace0c0024c9a
Nicolas Lindström <nicolaslindstrom1@gmail.com>
2025-10-16 09:40:06 -0700

    Update Get-ComputerServices.ps1

--- PowerShell/Get-ComputerServices/Get-ComputerServices.ps1
+++ PowerShell/Get-ComputerServices/Get-ComputerServices.ps1
@@ -1,11 +1,17 @@
-$Computers = ''
+function Get-ComputerService {
+    param (
+        [string]$computers
+        [string]$service
+    )
 
 foreach ($computer in $computers) {
 
-$pingtest = test-connection -computername $computer -count 1 -Quiet -erroraction SilentlyContinue
-if ($pingtest){
-	$servicestatus = (Get-Service -name * | fl -property Name, Status, StartType | out-string)
-	write-host $computer $servicestatus
+	$pingtest = test-connection -computername $computer -count 1 -Quiet -erroraction SilentlyContinue
+    if ($pingtest){
+     $servicestatus = (Get-Service -name $service | fl -property Name, Status, StartType | out-string)
+    write-host $computer $servicestatus
     }
-else {Write-Host($Computer + " is offline")}
+    else {Write-Host($Computer + " is offline")}
+}
+    
 }
