79d0669039c6644ecdf17d65becf84d9a06fe595
Nicolas Lindström <nicolaslindstrom1@gmail.com>
2026-08-01 10:33:56 -0700

    Refactor registry key script to use parameters

--- PowerShell/Custom Registry Key/regkey.ps1
+++ PowerShell/Custom Registry Key/regkey.ps1
@@ -1,12 +1,23 @@
-# Set the path
-$InstalledKey = ''
+function Set-InstalledFlag {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$InstalledKey,
 
-# Test the path if it doesn't exist create it. Installed software value is 1, change it to 0 for application uninstalled
-try {
-	Get-ChildItem -Path $InstalledKey -ErrorAction Stop
-	Set-ItemProperty -Path Registry::$InstalledKey -Name "Installed" -Value 1
-}
-catch {
-	New-Item -Path $InstalledKey
-	Set-ItemProperty -Path Registry::$InstalledKey -Name "Installed" -Value 1
+        [Parameter(Mandatory = $true)]
+        [string]$Value
+    )
+
+    # --- Manually set values here ---
+    $InstalledKey = ''
+    $Value = ''
+    # ---------------------------------
+
+    try {
+        Get-ChildItem -Path $InstalledKey -ErrorAction Stop
+        Set-ItemProperty -Path Registry::$InstalledKey -Name "Installed" -Value $Value
+    }
+    catch {
+        New-Item -Path $InstalledKey
+        Set-ItemProperty -Path Registry::$InstalledKey -Name "Installed" -Value $Value
+    }
 }
